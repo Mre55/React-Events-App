20f56e81c5e28f198217c43eeb80808afc71ea41
mre55 <misganawmihreteab>
2023-08-30 13:43:41 +0300

    Wrap all routes with queryclientProvider to listen to changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,43 +2,50 @@ import {
   Navigate,
   RouterProvider,
   createBrowserRouter,
-} from 'react-router-dom';
+} from "react-router-dom";
 
-import Events from './components/Events/Events.jsx';
-import EventDetails from './components/Events/EventDetails.jsx';
-import NewEvent from './components/Events/NewEvent.jsx';
-import EditEvent from './components/Events/EditEvent.jsx';
+import Events from "./components/Events/Events.jsx";
+import EventDetails from "./components/Events/EventDetails.jsx";
+import NewEvent from "./components/Events/NewEvent.jsx";
+import EditEvent from "./components/Events/EditEvent.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Navigate to="/events" />,
   },
   {
-    path: '/events',
+    path: "/events",
     element: <Events />,
 
     children: [
       {
-        path: '/events/new',
+        path: "/events/new",
         element: <NewEvent />,
       },
     ],
   },
   {
-    path: '/events/:id',
+    path: "/events/:id",
     element: <EventDetails />,
     children: [
       {
-        path: '/events/:id/edit',
+        path: "/events/:id/edit",
         element: <EditEvent />,
       },
     ],
   },
 ]);
 
+const queryClient = new QueryClient();
+
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
