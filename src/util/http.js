0758faa3c86842b1a09f2c0dfecf74bbc0b4011f
mre55 <misganawmihreteab>
2023-09-05 13:55:39 +0300

    Move queryClient to http to access it from every component

--- src/util/http.js
+++ src/util/http.js
@@ -1,15 +1,19 @@
+import { QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient();
+
 export async function fetchEvents({ signal, searchTerm }) {
   console.log(searchTerm);
-  let url = 'http://localhost:3000/events';
+  let url = "http://localhost:3000/events";
 
   if (searchTerm) {
-    url += '?search=' + searchTerm;
+    url += "?search=" + searchTerm;
   }
 
   const response = await fetch(url, { signal: signal });
 
   if (!response.ok) {
-    const error = new Error('An error occurred while fetching the events');
+    const error = new Error("An error occurred while fetching the events");
     error.code = response.status;
     error.info = await response.json();
     throw error;
@@ -20,18 +24,17 @@ export async function fetchEvents({ signal, searchTerm }) {
   return events;
 }
 
-
 export async function createNewEvent(eventData) {
   const response = await fetch(`http://localhost:3000/events`, {
-    method: 'POST',
+    method: "POST",
     body: JSON.stringify(eventData),
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
   });
 
   if (!response.ok) {
-    const error = new Error('An error occurred while creating the event');
+    const error = new Error("An error occurred while creating the event");
     error.code = response.status;
     error.info = await response.json();
     throw error;
@@ -43,10 +46,12 @@ export async function createNewEvent(eventData) {
 }
 
 export async function fetchSelectableImages({ signal }) {
-  const response = await fetch(`http://localhost:3000/events/images`, { signal });
+  const response = await fetch(`http://localhost:3000/events/images`, {
+    signal,
+  });
 
   if (!response.ok) {
-    const error = new Error('An error occurred while fetching the images');
+    const error = new Error("An error occurred while fetching the images");
     error.code = response.status;
     error.info = await response.json();
     throw error;
