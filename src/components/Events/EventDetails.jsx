42cd8c29a32af76b27df15b67f3ce6c1f6344e95
mre55 <misganawmihreteab>
2023-09-06 09:22:16 +0300

    Display waiting message if deleting is started

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -18,7 +18,12 @@ export default function EventDetails() {
     queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
   });
 
-  const { mutate } = useMutation({
+  const {
+    mutate,
+    isPending: isPendingDeletion,
+    isError: isErrorDeleting,
+    error: deleteError,
+  } = useMutation({
     mutationFn: deleteEvent,
     onSuccess: () => {
       queryClient.invalidateQueries({
@@ -108,13 +113,27 @@ export default function EventDetails() {
             undone.
           </p>
           <div className="form-actions">
-            <button onClick={handleStopDelete} className="button-text">
-              Cancel
-            </button>
-            <button onClick={handleDelete} className="button">
-              Delete
-            </button>
+            {isPendingDeletion && <p>Deleting, please wait...</p>}
+            {!isPendingDeletion && (
+              <>
+                <button onClick={handleStopDelete} className="button-text">
+                  Cancel
+                </button>
+                <button onClick={handleDelete} className="button">
+                  Delete
+                </button>
+              </>
+            )}
           </div>
+          {isErrorDeleting && (
+            <ErrorBlock
+              title="Failed to delete event"
+              message={
+                deleteError.info?.message ||
+                "Failed to delete event, please try again later."
+              }
+            />
+          )}
         </Modal>
       )}
       <Outlet />
